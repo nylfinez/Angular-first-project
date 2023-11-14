7a7675de70a530651a0ac95bf2f5262595d2e495
nyl <127328559+nylfinez@users.noreply.github.com>
2023-11-14 17:03:56 +0100

    Angular first project using tutorial ex.1-4

---
+++ first-app/src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 1999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
